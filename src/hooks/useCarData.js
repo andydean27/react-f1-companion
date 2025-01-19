2c10a56a05d679817ee05fda62700eb240d4ec5c
Andrew Dean <andydean27@gmail.com>
2025-01-19 20:47:43 +0000

    add car and stint data hooks

---
+++ src/hooks/useCarData.js
@@ -0,0 +1,44 @@
+// useLocationData.js
+import { useEffect, useState, useRef } from 'react';
+import { fetchOpenf1Data } from '../services/fetchSessionData';
+import { useCurrentTime, usePlayback } from '../contexts/Contexts';
+
+export const useLocationData = (sessionKey) => {
+    const [carData, setCarData] = useState([]);
+    const { isPlaying } = usePlayback();
+    const { currentTime } = useCurrentTime();
+    const currentTimeRef = useRef(currentTime);
+
+    useEffect(() => {
+        currentTimeRef.current = currentTime;
+    }, [currentTime]);
+
+    useEffect(() => {
+        if (!isPlaying || !sessionKey) return; // Skip interval creation if not playing
+        
+        const fetchData = async () => {
+            const data = await fetchOpenf1Data(
+                'car_data',             // endPoint
+                sessionKey,             // sessionKey
+                currentTimeRef.current, // currentTime
+                'date',                 // dateProperty
+                10000,                  // bufferUp
+                5000,                   // bufferDown
+                true);                  // log
+            
+            setCarData(data);
+        };
+
+        fetchData(); // fetch immediately when isPlaying is set to true
+
+        const intervalID = setInterval(fetchData, 5000);
+    
+        return () => {
+            console.log('Clearing car data interval...');
+            clearInterval(intervalID);
+         } // Cleanup on unmount or when dependencies change
+    }, [isPlaying, sessionKey]);
+    
+
+    return carData;
+};
