3288346bac721d4a93ef1e2516b9ba01588967c8
Andrew Dean <andydean27@gmail.com>
2025-01-19 20:46:35 +0000

    add colour maps for tyres

--- src/config/colourMaps.js
+++ src/config/colourMaps.js
@@ -9,3 +9,11 @@ export const segmentColourMap = {
     2064: "#2d2d2d",
     2068: "#2d2d2d"
 };
+
+export const compoundColourmap = {
+    "HARD": "#f4f4f4",
+    "MEDIUM": "#fffc58",
+    "SOFT": "#ea4337",
+    "INTERMEDIATE": "#69ca49",
+    "WET": "#3e8acd"
+}
